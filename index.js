444996e7051b9cf8d020b46835e245a806ebc954
ChromicQuanta <59491666+ChromicQuanta@users.noreply.github.com>
2024-10-31 09:52:46 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 http = require("http")
+fs = require("fs")
+
 http.createServer((q,s)=>{
   s.writeHeader(200,{"Content-Type":"text/html"})
-  s.end("<h1>Isondrite</h1>")
+ fs.readFile("index.html",(e,d)=>{
+   s.writeHeader(200,{"Content-Type":"text/html"})
+   s.end(d.toString())
+ })
 }).listen(1234)
