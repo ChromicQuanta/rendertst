31c8952a2130dc66d62aa8459471abf1cc3bedcb
ChromicQuanta <59491666+ChromicQuanta@users.noreply.github.com>
2024-10-31 09:57:27 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ fs = require("fs")
 http.createServer((q,s)=>{
   s.writeHeader(200,{"Content-Type":"text/html"})
  fs.readFile("index.html",(e,d)=>{
-   s.end(d.toString())
+   s.end("<h1>Schrodinger Equation:</h1>\n"+d.toString())
  })
 }).listen(1234)
