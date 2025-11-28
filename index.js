946426d34133a217b7c530303feb7e63a607dc63
ChromicQuanta <59491666+ChromicQuanta@users.noreply.github.com>
2025-11-28 23:01:49 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ fs = require("fs")
 http.createServer((q,s)=>{
   s.writeHeader(200,{"Content-Type":"text/html"})
  fs.readFile("index.html",(e,d)=>{
-   s.end(q.url+"<h1>Schrodinger Equation:</h1>\n"+d.toString())
+   if(q.url.includes(process.env.url)){
+     s.end(`Was too lazy to make a chatroom, so I made a <a href="${process.env.link}">discord server</a> instead lol`)
+   }else{
+   s.end("<h1>Schrodinger Equation:</h1>\n"+d.toString())
+   }
  })
 }).listen(1234)
