a1208fc6dd655e782cf05e4c7717ce4386e11d54
ChromicQuanta <59491666+ChromicQuanta@users.noreply.github.com>
2026-02-01 02:10:02 +1100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ http = require("http")
 fs = require("fs")
 
 http.createServer((q,s)=>{
+
+     if(q.url.includes("egechat.js")){
+        s.writeHeader(200,{"Content-Type":"application/javascript"})
+       fs.readFile("egechat.js",(e,d)=>{
+         s.end(d.toString())
+       })
+   }else{ 
+   s.end("<h1>Schrodinger Equation:</h1>\n"+d.toString())
+   
   s.writeHeader(200,{"Content-Type":"text/html"})
  fs.readFile("index.html",(e,d)=>{
    if(q.url.includes(process.env.url)){
@@ -10,4 +19,5 @@ http.createServer((q,s)=>{
    s.end("<h1>Schrodinger Equation:</h1>\n"+d.toString())
    }
  })
+}
 }).listen(1234)
